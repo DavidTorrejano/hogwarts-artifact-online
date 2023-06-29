4d65e82d09e7fbdf0947f519ac9d113516a79817
David Torrejano <davitorrejano@gmail.com>
2023-06-28 23:25:03 -0500

    This is the first commit

---
+++ src/test/java/com/edu/hogwartsartifactonline/HogwartsArtifactOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.hogwartsartifactonline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HogwartsArtifactOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
